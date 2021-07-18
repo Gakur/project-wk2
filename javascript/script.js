47744cb287ed19c5293664352348964711bd720e
Peter Gakure <petergakure97@gmail.com>
2021-07-18 14:00:47 +0300

    javascript

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+var CC, YY, MM, DD, d, dayvalue;
+var daynames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var femaleNames= ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+var maleNames= [""]

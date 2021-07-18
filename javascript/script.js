8fe17226376524750cd2d64b0f1a5d632d6c4ab9
Peter Gakure <petergakure97@gmail.com>
2021-07-18 15:31:35 +0300

    update

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,9 @@ var CC, YY, MM, DD, d, dayvalue;
 var daynames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var femaleNames= ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 var maleNames= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+
+function Validate(params) {
+    var gender= document.getElementsByName("gender");
+    if
+    
+}
